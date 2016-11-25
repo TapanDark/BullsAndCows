fa7d23812cb7a9ee7dbd10a202a2703f0444c0da
Tadarkar <tadarkar@nvidia.com>
2016-11-25 19:29:54 +0530

    A bunch of tester files

--- tester.py
+++ tester.py
@@ -5,7 +5,7 @@ import logging
 if __name__=='__main__':
     # logging.basicConfig(level=0)
     game = BullsAndCowsGame()
-    game.newGame(secret='9485')
+    game.newGame(secret='1232')
     player = BullsAndCowsPlayer()
     result = (0,0)
     while result[0]!=4:
--- tester3.py
+++ tester3.py
@@ -1,22 +1,22 @@
 from game import BullsAndCowsGame
 from player4 import BullsAndCowsPlayer
-import logging
+#import logging
 
 if __name__=='__main__':
-    #logging.basicConfig(level=0)
+    # logging.basicConfig(level=0)
     game = BullsAndCowsGame()
     player = BullsAndCowsPlayer()
     for i in range(0000,10000):
-        #if len(set(list('%04d'%i))) != 4:
-        #    continue
-        #else:
+        if len(set(list('%04d'%i))) != 4:
+            continue
+        else:
             result = (0,0)
             game.newGame(secret='%04d'%i)
             player.newGame()
             while result[0]!=4:
                 result=game.evaluate(player.guess())
                 player.processResult(result)
-            if game.attempts>20:
+            if game.attempts>9:
                 print("FAILING NUMBER %s"%i)
                 break
 
